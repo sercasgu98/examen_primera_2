2fc99a8f7ef3a6aa79e65bbf4c2e20aeb1e00f90
sergio <sercasgu98gmail.com>
2018-12-12 20:40:42 +0100

    Initial commit

---
+++ Assets/destruccion.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class destruccion : MonoBehaviour {
+    bool si = false;
+    public GameObject objeto;
+    private void Update()
+    {
+        if (si == true)
+        {
+            Destroy(objeto);
+        }
+           
+    }
+    void OnTriggerEnter(Collider other){
+        si = true;
+    
+    }
+
+}
